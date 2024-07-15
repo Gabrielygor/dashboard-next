26730dbc3a065278c8af7735c29d58a454fe6f63
Gabrielygor <gabrielcanuto042@gmail.com>
2024-07-15 07:00:42 -0300

    Hello

--- dashboard-text/src/app/page.tsx
+++ dashboard-text/src/app/page.tsx
@@ -1,11 +1,12 @@
-export default function Home() {
+
+
+
+export default function App() {
   return (
-    <main className="">
 
-      
-
-
-
+    <main className="h-screen flex-1 flex items-center justify-center">
+      <h1 className="text-zinc-50 size-4" >Hello World</h1>
     </main>
+
   );
 }
