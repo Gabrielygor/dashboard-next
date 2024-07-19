6e8600f77f7c48b2dbfae428f7a204306efcf384
Gabrielygor <gabrielcanuto042@gmail.com>
2024-07-19 08:19:05 -0300

    ADD FontAwesome dependences

--- project/src/components/navigation/navigation.tsx
+++ project/src/components/navigation/navigation.tsx
@@ -2,6 +2,9 @@
 
 import { useState } from "react";
 import style from "./navigation.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
 
 //Corrigir ? 
 
@@ -17,6 +20,9 @@ export function Navigation() {
             <div className={style.select_toggle} id="select-toggle" onClick={OpenNavigationMenu}>
                 <span className={style.select_toggle__select} id="state-select-toggle__state-select">Labican</span>
                 {/* <i className="fas fa-chevron-down state-select-toggle__icon" id="state-select-toggle__icon"></i> */}
+                <FontAwesomeIcon
+                    icon={faChevronDown}
+                />
             </div>
             <div className={`${style.select_list} ${isSelectMenuOpen ? style.select_list__show : ''}`} id="state-select-list">
                 <input type="text" name="" id="state-select-list__search" placeholder="Pesquisar" className={style.select_list__search} />
