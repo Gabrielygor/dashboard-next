60c9fad106188c78f677a8dbe95f352d839d571f
Gabrielygor <gabrielcanuto042@gmail.com>
2024-07-28 10:40:45 -0300

    add Theme Change function

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className="dark-mode ">
+    <html lang="pt-br">
       <body className={inter.className}>{children}</body>
     </html>
   );
